51b13577cb6db05e9d6f47489f2476c28987e2e9
Luthfi <vlootfie@gmail.com>
2018-01-29 13:14:58 +0700

    crud genre done

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const genreRouter = require('./routers/genreRouter');
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 
 app.use(bodyParser.json());
+
+app.use('/genres', genreRouter);
+
 app.listen(3000, () => console.log(`The App listening on port 3000!`));
