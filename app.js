26b26a0ce136c45d2baf50d33ef655ec09619466
Luthfi <vlootfie@gmail.com>
2018-01-30 11:02:50 +0700

    auth feature

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const genreRouter = require('./routers/genreRouter');
 const eventRouter = require('./routers/eventRouter');
+const authRouter = require('./routers/authRouter');
+const djRouter = require('./routers/dj');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
@@ -12,12 +16,17 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
+app.use(session({
+  secret: 'ilovescotchscotchyscotchscotch',
+  cookie: {
+    maxAge: 60000
+  }
+}));
+app.use(cookieParser());
 
 app.use('/genres', genreRouter);
 app.use('/events', eventRouter);
-
-const dj = require('./routers/dj');
-
-app.use('/dj', dj);
+app.use('/auth', authRouter);
+app.use('/dj', djRouter);
 
 app.listen(3000, () => console.log(`The App listening on port 3000!`));
