b579dabb81a8de7036e8813652eb48e920b12ea2
Luthfi <vlootfie@gmail.com>
2018-01-31 16:46:47 +0700

    send email to DJ and DJSeeker

--- models/book.js
+++ models/book.js
@@ -1,5 +1,6 @@
 'use strict';
 const emailSender = require('../helpers/emailSender');
+const isAccepted = require('../helpers/isAccepted');
 
 module.exports = (sequelize, DataTypes) => {
   var Book = sequelize.define('Book', {
@@ -14,26 +15,43 @@ module.exports = (sequelize, DataTypes) => {
   }
 
   Book.afterUpdate((book, options) => {
-    sequelize.models.DJ.findById(book.DJId)
-    .then(dj => {
-      sequelize.models.User.findById(dj.UserId)
-      .then(user => {
-        console.log('KIRIM EMAIL DISINI');
+    sequelize.models.Event.findById(book.EventId, {
+        include: [sequelize.models.DJSeeker]
+      })
+      .then(event => {
+        sequelize.models.User.findById(event.DJSeeker.UserId)
+          .then(user => {
+            let obj = {
+              email: user.email,
+              subject: 'Konfirmasi Pertunjukan Anda',
+              message: ` ${event.DJSeeker.name}, DJ yang anda pilih telah ${isAccepted(book.isApproved)} permintaan untuk mengisi acara anda`
+            }
+            let emailMessage = emailSender(obj);
+            console.log(emailMessage);
+          })
+          .catch(error => {
+            throw error
+          })
+      })
+      .catch(error => {
+        throw error
+      });
+
+    sequelize.models.DJ.findById(book.DJId, {
+        include: [sequelize.models.User]
+      })
+      .then(dj => {
         let obj = {
-          email: user.email,
-          subject: 'You have an email from findUrDije',
-          message: 'Cihuy'
+          email: dj.User.email,
+          subject: 'Konfirmasi Pertunjukan Anda',
+          message: ` ${dj.name}, anda telah ${isAccepted(book.isApproved)} permintaan untuk mengisi acara`
         }
         let emailMessage = emailSender(obj);
         console.log(emailMessage);
       })
       .catch(error => {
-        throw error
+        throw error;
       });
-    })
-    .catch(error => {
-      throw error;
-    });
   });
 
   return Book;
