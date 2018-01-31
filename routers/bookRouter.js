d46e877a57ce57c3acf4868e1a503e12fa6fc962
Luthfi <vlootfie@gmail.com>
2018-01-31 13:45:37 +0700

    DJ can reject or accept invitation

--- routers/bookRouter.js
+++ routers/bookRouter.js
@@ -7,24 +7,32 @@ const sessionChecker = require('../helpers/sessionChecker');
 router.get('/', sessionChecker, (req, res) => {
   djChecker(req.session.TypeId, (isDJ) => {
     if (isDJ) {
-      models.DJ.findOne({where:{UserId: req.session.UserId}})
-      .then(dj => {
-        models.Book.findAll({
-            where: {
-              DJId: dj.id
-            },
-            include: [models.Event]
-          })
-          .then(books => {
-            console.log(books);
-            res.render('./book/index', {title: 'Undangan'})
-          })
-          .catch(error => {
-            res.send(error);
-          })
-      })
-      .catch(error => {
-      });
+      models.DJ.findOne({
+          where: {
+            UserId: req.session.UserId
+          }
+        })
+        .then(dj => {
+          models.Book.findAll({
+              where: {
+                DJId: dj.id
+              },
+              include: [models.Event]
+            })
+            .then(books => {
+              console.log(books);
+              res.render('./book/index', {
+                title: 'Undangan',
+                books: books
+              })
+            })
+            .catch(error => {
+              res.send(error);
+            });
+        })
+        .catch(error => {
+          console.log(error);
+        });
     } else {
       res.redirect('/events');
     }
@@ -32,7 +40,39 @@ router.get('/', sessionChecker, (req, res) => {
 });
 
 router.get('/accept/:id', (req, res) => {
+  models.Book.findById(req.params.id)
+    .then(book => {
+      book.isApproved = true;
+      book.save()
+        .then(row => {
+          res.redirect('/books')
+          console.log(row);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
 
+router.get('/ignore/:id', (req, res) => {
+  models.Book.findById(req.params.id)
+    .then(book => {
+      book.isApproved = false;
+      book.save()
+        .then(row => {
+          res.redirect('/books')
+          console.log(row);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 module.exports = router;
