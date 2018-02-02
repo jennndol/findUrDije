27f657536a765e495b61ea485ed91dd44fec3438
Luthfi <vlootfie@gmail.com>
2018-02-02 08:06:09 +0700

    fix bug

--- routers/dj.js
+++ routers/dj.js
@@ -3,52 +3,77 @@ const express = require('express');
 const router = express.Router();
 const sessionChecker = require('../helpers/sessionChecker');
 
-router.get('/',sessionChecker, (req, res) =>{
+router.get('/', sessionChecker, (req, res) => {
   models.DJ.findAll({
     include: [models.User]
-  }).then(dataDj =>{
-    res.render('dj/dj', {title:'List DJ', dataDj: dataDj, session: req.session.username, isDJ: req.session.isDJ})
-  }).catch(err =>{console.log(err)})
+  }).then(dataDj => {
+    res.render('dj/dj', {
+      title: 'List DJ',
+      dataDj: dataDj,
+      session: req.session.username,
+      isDJ: req.session.isDJ
+    })
+  }).catch(err => {
+    res.send('ERROR 404')
+  })
 })
 
-router.get('/add', sessionChecker,(req, res) =>{
-  models.DJ.findAll().then(dataDj =>{
-    res.render('dj/addDj', {title:'Add DJ', dataDj: dataDj, session: req.session.username, isDJ: req.session.isDJ})
-  }).catch(err =>{console.log(err)})
+router.get('/add', sessionChecker, (req, res) => {
+  models.DJ.findAll().then(dataDj => {
+    res.render('dj/addDj', {
+      title: 'Add DJ',
+      dataDj: dataDj,
+      session: req.session.username,
+      isDJ: req.session.isDJ
+    })
+  }).catch(err => {
+    res.send('ERROR 404');
+  })
 })
 
-router.post('/add', (req, res) =>{
+router.post('/add', (req, res) => {
   let objDj = {
-    name : req.body.name,
-    phone : req.body.phone,
-    address : req.body.address,
-    detail : req.body.detail
+    name: req.body.name,
+    phone: req.body.phone,
+    address: req.body.address,
+    detail: req.body.detail
   }
-  models.DJ.create(objDj).then(dataDj =>{
+  models.DJ.create(objDj).then(dataDj => {
     res.redirect('/djs')
   })
 })
 
-router.get('/edit/:id', sessionChecker,(req, res) =>{
-  models.DJ.findById(req.params.id).then(dataDj =>{
-    res.render('dj/editDj', {title: 'Edit DJ', dataDj: dataDj, session: req.session.username, isDJ: req.session.isDJ})
-  }).catch(err => {console.log(err);})
+router.get('/edit/:id', sessionChecker, (req, res) => {
+  models.DJ.findById(req.params.id).then(dataDj => {
+    res.render('dj/editDj', {
+      title: 'Edit DJ',
+      dataDj: dataDj,
+      session: req.session.username,
+      isDJ: req.session.isDJ
+    })
+  }).catch(err => {
+    res.send('ERROR 404');
+  })
 })
 
-router.post('/edit/:id', sessionChecker,(req, res) =>{
+router.post('/edit/:id', sessionChecker, (req, res) => {
   let objDj = {
-    name : req.body.name,
-    phone : req.body.phone,
-    address : req.body.address,
-    detail : req.body.detail
+    name: req.body.name,
+    phone: req.body.phone,
+    address: req.body.address,
+    detail: req.body.detail
   }
-  models.DJ.update(objDj, {where: {id: req.params.id}}).then(dataDj =>{
+  models.DJ.update(objDj, {
+    where: {
+      id: req.params.id
+    }
+  }).then(dataDj => {
     res.redirect('/djs')
   })
 })
 
-router.post('delete/:id', sessionChecker,(req, res) =>{
-  models.DJ.destroy(req.params.id).then(dataDj =>{
+router.post('delete/:id', sessionChecker, (req, res) => {
+  models.DJ.destroy(req.params.id).then(dataDj => {
     res.redirect('/djs')
   })
 })
